d5118b0a950c8752d8216129dceac3964b5fc9d3
Deiontre10 <travontazlowry@gmail.com>
2023-01-02 17:11:35 -0500

    added questions for manager

--- index.js
+++ index.js
@@ -3,3 +3,34 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const team = [];
+
+function managerInfo () {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter a name for your manager?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Please enter an id for your manager?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter a email for your manager?"
+        },
+        {
+            type: "input",
+            name: "number",
+            message: "Please enter the office number for your manager?"
+        },
+    ]).then(function(response) {
+        const manager = new Manager(response.name, response.id, response.email, response.number);
+        team.push(manager);
+    })
+};
+managerInfo();
