c5c5061286d2b666270fd80a9d6ae54495ac51b3
Deiontre10 <travontazlowry@gmail.com>
2023-01-02 14:06:09 -0500

    updated index.js with inquirer and fs

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,17 @@
 const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
+    describe("Initialization", () => {
+        // it("Can create a test object", () => {
+        //     const employee = new Employee();
+        //     expect(typeof(employee).toBe("object"));
+        // });
+
+        // it("Can get name from constructer", () => {
+        //     const name = "Ash";
+        //     const employee = new Employee(name);
+        //     expect(employee.name.toBe(name));
+        // })
+    })
 
 });
