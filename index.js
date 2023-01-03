f438164a66d888ec75dee807faad7e1ae5f7aa8a
Deiontre10 <travontazlowry@gmail.com>
2023-01-02 19:18:17 -0500

    updated index.js and added template as a comment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
 const inquirer = require('inquirer');
 const fs = require('fs');
 
@@ -113,5 +114,7 @@ function internInfo () {
         employeeOption();
     })
 };
+
+
 
 managerInfo();
