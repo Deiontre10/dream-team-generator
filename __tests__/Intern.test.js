3ea90e6c2967d1e01ddf79f853c30644f3856600
Deiontre10 <travontazlowry@gmail.com>
2023-01-03 17:11:58 -0500

    updated Intern.test.js

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -2,4 +2,21 @@ const Intern = require("../lib/Intern");
 
 describe("Intern", () => {
 
+    it("Can set the school account from constructer", () => {
+        const school = "Duke";
+        const employee = new Intern("Ash", 10, "ash@ketchum.com", school);
+        expect((employee.school)).toBe(school);
+    });
+    
+    it("Can get the school account from getSchool()", () => {
+        const testSchool = "Duke";
+        const employee = new Intern("Ash", 10, "ash@ketchum.com", testSchool);
+        expect(employee.getSchool()).toBe(testSchool);
+    });
+
+    it("Can get the role from getRole() and return Intern", () => {
+        const testRole = "Intern";
+        const employee = new Intern("Ash", 10, "ash@ketchum.com", "Duke");
+        expect(employee.getRole()).toBe(testRole);
+    });
 });
