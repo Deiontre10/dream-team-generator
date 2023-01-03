66e0bcce8ae1b3b59312263ee47489685e0872d3
Deiontre10 <travontazlowry@gmail.com>
2023-01-03 16:30:31 -0500

    working on Employee.test.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,17 +1,27 @@
 const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
-    describe("Initialization", () => {
-        // it("Can create a test object", () => {
-        //     const employee = new Employee();
-        //     expect(typeof(employee).toBe("object"));
-        // });
 
-        // it("Can get name from constructer", () => {
-        //     const name = "Ash";
-        //     const employee = new Employee(name);
-        //     expect(employee.name.toBe(name));
-        // })
-    })
+    it("Can create a test object", () => {
+        const employee = new Employee();
+        expect(typeof(employee)).toBe("object");
+    });
 
+    it("Can set the name from constructer", () => {
+        const name = "Ash";
+        const employee = new Employee(name);
+        expect((employee.name)).toBe(name);
+    });
+    
+    it("Can get the name from getName()", () => {
+        const testName = "Ash";
+        const employee = new Employee(testName);
+        expect(employee.getName()).toBe(testName);
+    });
+
+    it("Can set the id from constructer", () => {
+        const id = 10;
+        const employee = new Employee("ash", id);
+        expect((employee.id)).toBe(id);
+    });
 });
