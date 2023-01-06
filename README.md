a32ed0b553f442b56f19c13be8e45a9e0100ddb0
Deiontre10 <travontazlowry@gmail.com>
2023-01-06 16:39:50 -0500

    updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # dream-team-generator
+[Link to my project](https://github.com/Deiontre10/dream-team-generator)
+
+## Table of Contents
+- [Description](#description)
+- [Visuals](#visuals)
+
+## Description
+Dream Team Generator is a tool that helps to create and organize teams. You first add your manager and then as many engineers and interns as you would like. After you have added all members you then select finish to generate your page with your teams info.
+
+## Visuals
+
+![An image of the generated HTML](assets/main.png "Generated Dream Team")
--- src/page-template.js
+++ src/page-template.js
@@ -1,7 +1,6 @@
 
 const generateManagers = (team) => {
-  return `
-  <article>
+  return `<article>
     <h2>Name: ${team.name}</h2>
     <ul>
       <li>ID: ${team.id}</li>
@@ -13,8 +12,7 @@ const generateManagers = (team) => {
 };
 
 const generateEngineers = (team) => {
-  return `
-  <article>
+  return `<article>
     <h2>Name: ${team.name}</h2>
     <ul>
       <li>ID: ${team.id}</li>
@@ -25,8 +23,7 @@ const generateEngineers = (team) => {
 };
 
 const generateInterns = (team) => {
-  return `
-  <article>
+  return `<article>
     <h2>Name: ${team.name}</h2>
     <ul>
       <li>ID: ${team.id}</li>
